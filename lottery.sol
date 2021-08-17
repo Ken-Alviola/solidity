ba5269d12c1b20687a42b43b2f7ee56157eb0ce4
Ken-Alviola <kenalviola@gmail.com>
2021-08-17 15:29:38 -0500

    added code to reset players array

--- lottery.sol
+++ lottery.sol
@@ -36,6 +36,7 @@ contract Lottery{
         winner = players[index];
         
         winner.transfer(getBalance());
+         players = new address payable[](0); //resetting the Lottery
     }
     
 }
