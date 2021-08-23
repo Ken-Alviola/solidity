03a5647f0d518f28d5f3aef390d5807091df5b80
Ken-Alviola <kenalviola@gmail.com>
2021-08-23 13:57:17 -0500

    adding state variables

--- auction.sol
+++ auction.sol
@@ -3,5 +3,16 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Auction{
+    address payable public owner;
+    uint public startBlock;
+    uint public endBlock;
+    string public ipfsHash;
     
+    enum State {Started, Running, Ended, Canceled}
+    State public auctionState;
+    
+    uint public highestBindingBid;
+    address payable public highestBidder;
+    
+    mapping(address => uint) public bids;
 }
