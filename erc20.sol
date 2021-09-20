651cc77aa679c396da79a4fa49f692ac71ec0859
Ken-Alviola <kenalviola@gmail.com>
2021-09-20 14:43:20 -0500

    adding erc20 comments

--- erc20.sol
+++ erc20.sol
@@ -1,3 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.6.0 <0.9.0;
+// ----------------------------------------------------------------------------
+// EIP-20: ERC-20 Token Standard
+// https://eips.ethereum.org/EIPS/eip-20
+// -----------------------------------------
