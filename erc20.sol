1d4ee8d90289b89f3fedd702c0f5564506706dc2
Ken-Alviola <kenalviola@gmail.com>
2021-09-24 16:27:15 -0500

    adding constructor and balanceOf and transfer functions

--- erc20.sol
+++ erc20.sol
@@ -7,12 +7,12 @@ interface ERC20Interface {
     function balanceOf(address tokenOwner) external view returns (uint balance);
     function transfer(address to, uint tokens) external returns (bool success);
     
-    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-    function approve(address spender, uint tokens) external returns (bool success);
-    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+    //function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    //function approve(address spender, uint tokens) external returns (bool success);
+    //function transferFrom(address from, address to, uint tokens) external returns (bool success);
     
     event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    //event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
  
  
@@ -26,4 +26,23 @@ contract Cryptos is ERC20Interface{
     mapping(address => uint) public balances;
     // balances[0x1111...] = 100;
     
+    constructor(){
+        totalSupply = 1000000;
+        founder = msg.sender;
+        balances[founder] = totalSupply;
+    }
+    
+    function balanceOf(address tokenOwner) public view override returns (uint balance){
+        return balances[tokenOwner];
+    }
+    
+    function transfer(address to, uint tokens) public override returns(bool success){
+        require(balances[msg.sender] >= tokens);
+        
+        balances[to] += tokens;
+        balances[msg.sender] -= tokens;
+        emit Transfer(msg.sender, to, tokens);
+        
+        return true;
+    }
 }
